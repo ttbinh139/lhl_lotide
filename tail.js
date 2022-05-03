3be96322199726980757fbe5333fcdcb306daac8
ttbinh139 <ttbinh139@gmail.com>
2022-05-03 23:02:35 +0000

    Add function eqArrays to compare two arrays

--- tail.js
+++ tail.js
@@ -19,4 +19,3 @@ const tail = function(data) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
