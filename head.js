8562b42da863c80820c14e87d80c4cb58eb5ce09
ttbinh139 <ttbinh139@gmail.com>
2022-05-09 22:10:32 +0000

    Iniitial test folder, separate testing code for assertEqual and head function

--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Fail: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(data) {
   let head;
   if (data.length > 0 && Array.isArray(data) === true) {
@@ -15,5 +7,4 @@ const head = function(data) {
   return head;
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
