269cf4521be8f37781a37b8a239d6d7b0272e646
ttbinh139 <ttbinh139@gmail.com>
2022-05-09 22:16:22 +0000

    Refactor testing code for eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Fail: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,5 +11,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], []), true);
+module.exports = eqArrays;
